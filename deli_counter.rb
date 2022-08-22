f6634c760652eb4a21bfc0d47b8544e147916a41
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-08-22 13:33:50 +0000

    Completed lab

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
+# require "pry"
 # Write your code here.
+katz_deli = ["Ada", "Grace"]
+
+def line kats_deli
+  if kats_deli.empty?
+    puts "The line is currently empty."
+  else 
+    
+   puts "The line is currently: #{
+      array_name = kats_deli.map.with_index (1) do |item, index| 
+        "#{index}. #{item}"
+      end
+      array_name.join(" ")
+    }"
+  end
+end
+
+def take_a_number (kats_deli,new_person)
+  kats_deli << new_person
+
+  puts "Welcome, #{new_person}. You are number #{kats_deli.length} in line."
+end
+
+def now_serving (kats_deli)
+  if kats_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{kats_deli.first}."
+    kats_deli.shift
+  end
+end
+
+# line(["Ada", "Grace"])
+# binding.pry
+
+
